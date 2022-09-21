d63dc9e0f8c82b59740efd5241ed18a62ab249f7
kostyaspartak89 <kostyaspartak89@gmail.com>
2022-09-21 14:45:28 +0900

    addscards

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,33 @@
-function newCard(number) {
-  let card = document.createElement('div');
-  card.classList.add('card');
-  card.textContent = number;
-  card.addEventListener('click', function flip() {
-        card.classList.add('open')
-      })
-  document.getElementById('game').append(card);
-}
-
-
+let card;
+let cardNumber;
 let cardsAmount = 6,
     cardsNumberArray = [],
-    cardsArray = []
-function newGame(cardsAmount) {
-  for( i = 1; i <= cardsAmount / 2; i++) {
-    cardsNumberArray.push(i)
-    cardsNumberArray.push(i)
-  }
-  cardsNumberArray = cardsNumberArray.sort(() => Math.random() - 0.5);
-  for (const cardNumber of cardsNumberArray) {
-    cardsArray.push(cardNumber)
-  }
+    cardsArray = [],
+    cardsAddarr = []
+
+function startGame() {
+  function getCard(number) {
+    for (j = 1; j <= cardsAmount; j++) {
+      let card = document.createElement('div');
+      card.classList.add('card');
+      card.textContent = number;
+      card.addEventListener('click', function flip() {
+        card.classList.add('open')
+      })
+      for ( i = 1; i < cardsAmount / 2; i++) {
+        cardsNumberArray.push(i)
+        cardsNumberArray.push(i)
+      }
+      cardsNumberArray = cardsNumberArray.sort(() => Math.random() - 0.5);
+      for (const cardNumber of cardsNumberArray) {
+        cardsArray.push(cardNumber)
+      }
+      for (k = 1; k < cardsNumberArray.length; k++) {
+        cardsAddarr.push(k)
+      }
+      document.getElementById('game').append(card);
+    }
+  }console.log(cardsAddarr)
+  return getCard(cardsArray.push(card))
 }
-
-newGame(document.getElementById('game'), cardsAmount)
-
+startGame()
